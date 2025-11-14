98b29251104a624b65edd9c27d33ef9fa5854d26
dastanazkael <dastan.azkael@gmail.com>
2025-11-14 23:58:33 +0700

    Add files via upload

---
+++ app/src/main/java/com/momo83/kasir/MainActivity.java
@@ -0,0 +1,10 @@
+package com.momo83.kasir;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle b){
+        super.onCreate(b);
+    }
+}
